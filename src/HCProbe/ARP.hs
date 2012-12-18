20e0f87940b8c4ccbb55ed622db7d081d96989f7
Dmitry Zuikov <dzuikov@gmail.com>
2012-12-18 13:48:09 +0400

    missed files

---
+++ src/HCProbe/ARP.hs
@@ -0,0 +1,29 @@
+module HCProbe.ARP where
+
+import Network.Openflow.Types
+import Network.Openflow.Ethernet.Types
+import Network.Openflow.Ethernet.ARP
+
+data ARPGratuitousReply = ARPGratuitousReply { senderMAC :: MACAddr
+                                             , senderIP  :: IPv4Addr
+                                             }
+
+instance ARPReply ARPGratuitousReply where
+  hardwareType    x = 0x01
+  ipProtocolType  x = 0x800
+  hwAddressLen    x = 6
+  ipAddressLen    x = 4
+  opcode          x = 0x02
+  senderHWAddress = senderMAC
+  senderIPAddress = senderIP
+  targetHWAddress = senderMAC
+  targetIPAddress = senderIP
+
+
+instance EthernetFrame ARPGratuitousReply where
+  dstMacAddress  _ = 0xFFFFFFFFFFFF
+  srcMacAddress    = senderMAC
+  vlanID         _ = Nothing
+  typeCode       _ = 0x806
+  putPayload       = putARPReply
+
