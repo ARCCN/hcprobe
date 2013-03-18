795eb923e35be4a07222a8f1f304a97fc7fed88e
Shved <shved@lvk.cs.msu.su>
2013-03-18 17:41:09 +0400

    shved_branch: starting
    branch for improve OF message generation and sending

--- src/testClient.hs
+++ src/testClient.hs
@@ -1,5 +1,4 @@
 module Main ( main
-            , client
             ) where
 
 import System.Environment
@@ -44,7 +43,7 @@ fsMacsPerPort       = 1000
 fsPorts             = 48
 
 pktSendTimeout      = 1
-pktSendNumber       = 10000
+pktSendNumber       = 1000000
 
 pktMsgOfVersion     = 1 :: Word8
 pktMsgOfSwitch      = 1 :: Word32
@@ -75,25 +74,35 @@ pktMsgActionType    = FS.defActions
 pktMsgOfpPhyPorts   = []
 
 pktSendMsg :: OfpMessage
-pktSendMsg = OfpMessage ( header pktMsgOfVersion pktMsgOfSwitch OFPT_FEATURES_REPLY)
-                    ( OfpFeatureReply $ OfpSwitchFeatures 1000 100 1 (S.fromList pktMsgCapabuilities) (S.fromList pktMsgActionType) pktMsgOfpPhyPorts )
+pktSendMsg = OfpMessage hd dt
+    where
+        hd = header pktMsgOfVersion pktMsgOfSwitch OFPT_FEATURES_REPLY
+        dt = OfpFeatureReply $ OfpSwitchFeatures 1000 100 1 cb at pktMsgOfpPhyPorts
+        cb = S.fromList pktMsgCapabuilities
+        at = S.fromList pktMsgActionType 
 
-client fk@(FS.FakeSwitch sw switchIP _ sH rH) ad = replicateM_ pktSendNumber $ do
+ofClient fk@(FS.FakeSwitch sw switchIP _ sH rH) ad = replicateM_ pktSendNumber $ do
         sendReplyT pktSendMsg
         M.threadDelay pktSendTimeout
---        print "AAAAAAA!"
 
     where
         sendReplyT msg = do
         liftIO $ FS.dump "OUT:" (ofp_header msg) replyBs
+        --print $ BS8.length replyBs
         yield replyBs $$ (appSink ad)
         maybe (return ()) (\x -> (liftIO.x) msg) sH
         where replyBs = FS.encodeMsg msg
 
-ofpClient sw host port = runTCPClient (clientSettings port host) (client sw)
+zeroClient ad = replicateM_ pktSendNumber $ do
+    yield replyBs $$ (appSink ad)
+    where 
+        replyBs = BS.replicate 32 $ 0
+
+ofpClient sw host port client = runTCPClient (clientSettings port host) client
 
 main = do
-    (host,port) <- host_port
+    args <- getArgs
+    let (host,port) = host_port args
     fakeSw <- do
         let i = 100
         let ip = fromIntegral i .|. (0x10 `shiftL` 24)
@@ -102,8 +111,9 @@ main = do
         return $ fst $ FS.makeSwitch (FS.defaultSwGen i ip rnd) 
                         (fromIntegral fsPorts) 
                         macs [] FS.defActions [] [] [OFPPF_1GB_FD,OFPPF_COPPER]
+    let client = take_client fakeSw args
     initTime <- getCurrentTime
-    ofpClient fakeSw (BS8.pack host) (read port)
+    ofpClient fakeSw (BS8.pack host) (read port) client
     now <- getCurrentTime
 
     putStr "Time for all is: "
@@ -112,11 +122,8 @@ main = do
     putStr "Time per package: "
     print $ timeDif / (fromIntegral pktSendNumber)
     where
-        host_port = do 
-            args <- getArgs
-            if (length args >= 2)
-                then return ( head args, head $ tail args)
-                else if (length args == 1)
-                        then return ( head args, fsDefaultPort)
-                        else return ( fsDefaultHost, fsDefaultPort)
-
+        host_port (host:port:_) = (host,port)
+        host_port (host:[]) = (host, fsDefaultPort)
+        host_port [] = (fsDefaultHost, fsDefaultPort)
+        take_client _ (_:_:"-0":_) = zeroClient
+        take_client fs _ = ofClient fs
