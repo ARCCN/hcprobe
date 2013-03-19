2fb0c5beff030fc6d86f6c10edf6195ccd3c7929
Dmitry Zuikov <dmitry@zuikov.me>
2013-03-19 10:56:01 +0400

    Ethernet frames generation speedup, no CRC yet

--- src/TestMisc.hs
+++ src/TestMisc.hs
@@ -7,8 +7,9 @@ import Network.Openflow.Ethernet.Types
 import Network.Openflow.Misc
 import qualified Data.ByteString as BS
 import qualified Data.Binary.Put as BP
+import Data.Binary.Get
 
-test_putMAC = TestCase (assertEqual "macEqual" macStrict macBS)
+test_putMAC1 = TestCase (assertEqual "macEqual" macStrict macBS)
   where macStrict = runPutToByteString 16 $ putMAC 0xFFFFAABBCCDDEEFF
         macBS     = bsStrict $ BP.runPut $ do BP.putWord8 0xAA
                                               BP.putWord8 0xBB
@@ -17,7 +18,7 @@ test_putMAC = TestCase (assertEqual "macEqual" macStrict macBS)
                                               BP.putWord8 0xEE
                                               BP.putWord8 0xFF
 
-allTests = TestList [TestLabel "putMAC" test_putMAC
+allTests = TestList [TestLabel "putMAC#1" test_putMAC1
                     ]
 
 main = runTestTT allTests 
